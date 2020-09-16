c1fbfadb10fe7f6bdf78343aae640bc0e7cefe65
belon-creator <belon8911@gmail.com>
2020-09-16 21:34:56 +0300

    add structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import 'modern-normalize/modern-normalize.css';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
